22c006ba07d3bb0acbae856087abc969f4e3390b
Hasan KAYA <hasankaya3546@hotmail.com>
2025-04-28 21:40:28 +0300

    integers ve string dersleri tamamlandi

---
+++ 2. Ders/strings.py
@@ -0,0 +1,48 @@
+# "ali"
+
+# yazi1 = 'nova akademi'
+# print("yazi1: ", yazi1)
+
+# yazi2 = "nova akademi"
+# print("yazi2: ", yazi2)
+
+# yazi3 = """nova akademi"""
+# print("yazi3: ", yazi3)
+
+# yazi1 = 'merhaba dünya"
+
+# yazi1 = 'Selcuk\'un bugün dersi var'
+# print(yazi1)
+
+# a = "alidfghjkkjhgfdsfvhjdfbjd"
+# print(a[-3])
+
+# string parçalama işlemleri
+# yazi = "ben bir gazete yazısıyım."
+# '[başlama indeksi : bitiş indeksi : atlama değeri]'
+# print(yazi[4:14])
+# print(yazi[:14])
+# print(yazi[4:])
+# print(yazi[4:-1])
+# print(yazi[::3])
+# print(yazi[4:14:2])
+# tersten yazdırma
+# print(yazi[::-1])
+
+# yazi = "ali ata bak."
+# print(len(yazi))
+# print(yazi[2])
+# yazi[2] = "o"
+
+# yazi = "alo ata bak."
+# print(yazi)
+
+# string toplama
+# a = "Python "
+# b = "Programlama "
+# c = "Dili "
+# print(a + b + c)
+
+# string çarpma
+yazi = "Python"
+print(yazi * 5)
