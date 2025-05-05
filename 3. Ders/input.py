93635cbd32921c9db6abd82bd95e16cd20aa3495
Hasan KAYA <hasankaya3546@hotmail.com>
2025-05-05 20:46:57 +0300

    input, listeler ve print fonksiyonu dersleri tamamlandi

---
+++ 3. Ders/input.py
@@ -0,0 +1,26 @@
+# input() fonksiyonu kullanicidan girdi almak icin kullanilir
+# input()
+
+# input("Lütfen bir sayı giriniz: ")
+
+# a = input("Lütfen bir sayı giriniz: ")
+# print("Kullanıcının girdiği değer: ", a)
+
+# a = input("Lütfen bir sayı giriniz: ")
+# print(type(a))
+# print(a*3)
+# a = int(a)
+# print(type(a))
+# print(a*3)
+
+# a = input("Lütfen bir sayı giriniz: ")
+# print(type(a))
+# print(a*3)
+# a = float(a)
+# print(type(a))
+# print(a*3)
+
+sayi1 = int(input("Birinci sayiyi giriniz: "))
+sayi2 = int(input("İkinci sayiyi giriniz: "))
+sayi3 = int(input("Üçüncü sayiyi giriniz: "))
+print("Verdiğiniz sayilarin toplamlari = ", sayi1 + sayi2 + sayi3)
---
+++ 3. Ders/print_fonk.py
@@ -0,0 +1,37 @@
+# print(35)
+# print(3.14)
+# a = 4
+# b = 15
+# print(a + b)
+# print("Hasan Kaya")
+# print("Selcuk'un bugün dersi var")
+
+# print("Nova", 12, 567, 78777, 3.57)
+# print("Nova", "Akademy", "1990")
+
+# print("Nova\nAcademy\nPython.")
+
+# type fonksiyonu -> veri tipini öğrenmek için
+a = 3.14
+print(type(a))
+
+# sep parametresi
+# print(11, 22, 33, 44, sep="**")
+# print("06", "04", "2024", sep="/")
+# print("Python", "Nova", "Akademi", sep="\n")
+
+# * parametresi
+# print(*"Python", sep="\n")
+# print("P", "y", "t", "h", "o", "n")
+# print(*"TBMM", sep=".")
+
+# formatlama
+# print("{} {} {}".format(3.14, 5.32, 7.56))
+
+# format fonksiyonu örnek:
+# sayi1 = 5
+# sayi2 = 4
+# print("{} + {} = {}".format(sayi1, sayi2, sayi1 + sayi2))
+# print("{2} {0} {1}".format(43, "Selçuk", 54))
+
+# print("{:.2f} {:.2f} {:.3f}".format(3.51465465, 8.7894564, 9.41556652))
