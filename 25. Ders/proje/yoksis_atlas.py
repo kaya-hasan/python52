53e049bc9c14133456a60fce7a91d6edfb047dd0
Hasan KAYA <h3kaya5@gmail.com>
2025-08-10 22:57:46 +0300

    10.08.25

--- 25. Ders/proje/yoksis_atlas.py
+++ 25. Ders/proje/yoksis_atlas.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from googletrans import Translator
+import asyncio
+
+translator = Translator()
 
 
 def get_university_data(url):
@@ -13,11 +17,25 @@ def get_university_data(url):
         for li_tag in li_tags:
             data_id = li_tag.find("a", href=True)["data-id"]
             uni_name = li_tag.find("h3", class_="baslik").text.strip()
+
+            async def translate_text():
+                async with Translator() as translator:
+                    result = await translator.translate(uni_name, src='tr', dest='en')
+                    return result
+            uni_name_en = asyncio.run(translate_text())
             university_data.append({
                 "id": data_id,
-                "name": uni_name
+                "name": uni_name,
+                "name_en": uni_name_en.text
             })
         return university_data
+
+
+def save_to_txt(data):
+    with open('university_data.txt', 'w', encoding='utf-8') as file:
+        file.write("Id, Name\n")
+        for uni in data:
+            file.write(f"{uni['id']}, {uni['name']}, {uni['name_en']}\n")
 
 
 url = "https://yokatlas.yok.gov.tr/universite.php"
@@ -26,3 +44,5 @@ print(university_data)
 
 for uni in university_data:
     print(f"ID: {uni['id']}, Name: {uni['name']}")
+
+save_to_txt(university_data)
