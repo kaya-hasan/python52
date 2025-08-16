b09eaa5c7da0dce785adc43e252b4993c8bc2001
Hasan KAYA <h3kaya5@gmail.com>
2025-08-16 23:51:18 +0300

    16.08.25

---
+++ blog/article/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Article
+# Register your models here.
+
+# admin.site.register(Article)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_date"]
+    list_display_links = ["title", "created_date"]
+    search_fields = ["title"]
+    list_filter = ["title", "author"]
+    ordering = ["title"]
+
+    class Meta:
+        model = Article
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Istanbul'
 
