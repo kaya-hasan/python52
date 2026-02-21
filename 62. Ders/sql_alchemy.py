d1eebe5776c3f28dcca055cc42dea6796f9edd35
Hasan KAYA <hasankaya3546@hotmail.com>
2026-02-21 22:08:37 +0300

    Update README and lesson 62 summary

--- 62. Ders/sql_alchemy.py
+++ 62. Ders/sql_alchemy.py
@@ -51,5 +51,32 @@ class Gonderi(db.Model):
         return f"Gonderi('{self.baslik}', '{self.tarih}')"
 
 
+with app.app_context():
+    db.create_all()
+    # CREATE - yeni kayıt
+
+    kullanici1 = Kullanici(kullanici_adi='emre',
+                           email='emre@ornek.com', sifre='hashed_password')
+    kullanici2 = Kullanici(kullanici_adi='batuhan',
+                           email='batuhan@ornek.com', sifre='2hashed_password')
+    # isteğe bağlı çoklu kayıtlar eklenebilir -- add_all kodunu kullanmamız bir liste alıp hepsini eklememizi sağlar
+    db.session.add_all([kullanici1, kullanici2])
+    db.session.commit()
+
+    # READ - okuma
+    tum_kullanicilar = Kullanici.query.all()
+    ilk_kullanici = Kullanici.query.first()
+    kullanici = Kullanici.query.filter_by(kullanici_adi='istenenIsim').first()
+    # ID numarası içeri yazılır ve o ID çekilir
+    kullanici = Kullanici.query.get(1)
+
+    # UPDATE - güncelleme
+    kullanici = Kullanici.query.get(1)  # id ye göre getirdik
+    kullanici = email = 'yenimail@email.com'
+    db.session.commit()
+
+    # DELETE - silme
+    kullanici = Kullanici.query.get(1)  # id ye göre getirilir
+    db.session.delete(kullanici)
 if __name__ == "__main__":
     app.run(debug=True)
